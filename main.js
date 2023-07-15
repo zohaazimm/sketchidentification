ad88991ae61d1dafd9bc97bd33f4c4aa3c46d1a0
zohaazimm <139622263+zohaazimm@users.noreply.github.com>
2023-07-15 11:02:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function clearCanvas() {
+    background("white");
+}
+
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw() {
+strokeWeight(13);
+stroke(0);
+
+if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+}
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0]).label;
+    synth.speak(utterThis);
+}
+
